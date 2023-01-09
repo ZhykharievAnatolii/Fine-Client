ae09afea23f11f36b7be4a7f32aa590831a1d1c1
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-09 18:07:18 +0200

    finish validation

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ document.addEventListener('DOMContentLoaded',()=>{
     const registerForm=document.querySelector('.register-form');
     registerForm.addEventListener('submit',(event)=>{
         event.preventDefault();
-
+        // [...registerForm.elements].forEach((elem)=>{
+        //     validate(elem)
+             const isValid  = [...registerForm.elements].reduce((accum,elem)=>{
+                const result=elem.matches('input')?validate(elem):true;
+                return !accum? accum:result;
+        }, true);
+        if(isValid){
+            alert('Success')
+        }
     });
 
     registerForm.addEventListener('change',({target})=>{
