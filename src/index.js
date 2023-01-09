43ecba37ad5ea460606579d8b49d8fdeec909747
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-09 12:58:53 +0200

    add date-fns

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
 
 import './scss/index.scss';
 
-createRoot(document.querySelector('.root')).render(
-  <React.StrictMode>
-    <div>Blablabla</div>
-  </React.StrictMode>
-);
