fdf61b6c56da1d5f412e7a7c97d091b1b71f7ba6
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-12 14:56:20 +0200

    upd headers

--- src/js/view/header.js
+++ src/js/view/header.js
@@ -1,5 +1,25 @@
+import {viewController} from "./ViewController";
+
 class Header {
     header=document.querySelector('.navbar');
+
+    constructor() {
+        this.header.addEventListener('click', (event) => {
+            event.preventDefault();
+            const {target} = event;
+            if (target.dataset.page) {
+                const page = target.dataset.page;
+                if (page === 'login') {
+                    return viewController.openLoginPage();
+                }
+                if (page === 'registration') {
+                    return viewController.openRegistrationPage();
+                }
+                viewController.openDashBoard();
+            }
+        })
+    }
+
 
     openLoggedInView(){
         this.header.querySelectorAll('.navbar-nav a').forEach((item)=>{
