b952b23d4815e1e35ea5e5a59f34de5da78449f5
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-12 14:26:16 +0200

    upd styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap';
 import {validateForm} from "./js/utils/validate";
 import {loginUser, registerUser} from "./js/api";
 import {viewController} from "./js/view/ViewController";
-import {b} from "msw/lib/glossary-58eca5a8";
+// import {b} from "msw/lib/glossary-58eca5a8";
 import {store} from "./js/store";
 
 document.addEventListener('DOMContentLoaded',()=>{
