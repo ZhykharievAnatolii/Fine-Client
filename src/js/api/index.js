535953c8bda607bc415102cfb748af68acdaed47
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-15 19:59:48 +0200

    optimize api

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -5,14 +5,25 @@ export const client= axios.create({
     baseURL:'http://localhost:3000/api'
 });
 
-export const registerUser=async (body,)=>{
-    const {data}=await client.post('/register',body);
-    //     ,{
-    //     headers
-    // }
-    return data;
-};
-export const loginUser=async (body,)=>{
-    const {data}=await client.post('/login',body);
-    return data;
-};
+export const registerUser=async (body,)=>
+await client.post('/register',body);
+/**
+ * @param body
+ * returns {Promice<axios.Axios.response<{token:string,email:string}>>}
+ */
+
+export const loginUser=async (body,)=>
+    await client.post('/login',body);
+
+
+// export const registerUser=async (body,)=>{
+//     const {data}=await client.post('/register',body);
+//     //     ,{
+//     //     headers
+//     // }
+//     return data;
+// };
+// export const loginUser=async (body,)=>{
+//     const {data}=await client.post('/login',body);
+//     return data;
+// };
