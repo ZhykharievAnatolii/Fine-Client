cb19ba5cc7a2896da0dac127ddfc685a60cf43d7
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-16 12:11:07 +0200

    upd date validat

--- src/js/view/renderfines.js
+++ src/js/view/renderfines.js
@@ -1,20 +1,22 @@
+import {format, formatDistance} from "date-fns";
+
+
 const list=document.querySelector('.finest-list')
 
 export function renderFines({id,amount,description,date,deadLine,paid}) {
     const item = document.createEvent('li');
     item.innerHTML=`<li class="card" style="width: 18rem;" data-id=`${id}`>
         '        <div class="card-body">
-        '          <h5 class="card-title">${amount}</h5>'
+        '          <h5 class="card-title">$${amount}</h5>'
         '          <p class="card-text">${description}</p>'
         '        </div>' 
         '        <ul class="list-group list-group-flush">' 
-        '          <li class="list-group-item">${data}</li>' 
+        '          <li class="list-group-item">${format(new Date(date), yyyy, MM, dd)}</li>' 
         '          <li class="list-group-item">${deadLine}</li>' 
         '          <li class="list-group-item">${paid}</li>' 
         '        </ul>\
         '        <div class="card-body">
-        '          <a href="#" class="card-link">Card link</a>\
-        '          <a href="#" class="card-link">Another link</a>
+        '          <button class="btn btn-primary">Pay</button>
         '        </div>\n'
         '      </li>`;
             list.append(item)
